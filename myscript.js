b61719991d1cd2623864c2d180cf005145d643de
Giuseppe Mele <giuseppemele9410@gmail.com>
2025-12-01 16:22:29 +0100

    feat : stampo prezzo su pagina html

--- myscript.js
+++ myscript.js
@@ -10,33 +10,41 @@
 let basicPrice ; 
 let discount ; 
 let finalPrice ; 
+let message ; 
 const pricePerKm = 0.21; 
 
 // INPUT 
 const numberOfKm = parseFloat(prompt("Inserisci il numero di Km: ")); 
 const age = parseInt(prompt("Inserisci la tua età: ")); 
-console.log("Kilometri da percorrere: ",numberOfKm) ; 
+
+// OUTPUT DATI INSERITI 
+console.log("Kilometri da percorrere:", numberOfKm) ; 
 console.log("Età passeggero:", age); 
 
 // CALCOLO IL PREZZO BASE IN BASE ALL'INPUT 
 basicPrice =  numberOfKm * pricePerKm ; 
-console.log("Prezzo base : " , basicPrice.toFixed(2) ,"€"); 
+console.log("Prezzo base:", basicPrice.toFixed(2) ,"€"); 
 
 if(age < 18) {
     discount = basicPrice * 0.20 ; // sconto 
     finalPrice = basicPrice - discount ; // prezzo finale 
-    console.log("Prezzo scontato UNDER 18:",finalPrice.toFixed(2), "€"); 
+    message = "Ti è stato applicato uno sconto del 20% perché sei minorenne."; 
+    //console.log("Prezzo scontato UNDER 18:",finalPrice.toFixed(2), "€"); 
 }
 else if (age > 65) {
     discount = basicPrice * 0.40;
     finalPrice = basicPrice - discount ;
-    console.log("Prezzo scontato OVER 65:",finalPrice.toFixed(2), "€");
+    message = "Ti è stato applicato uno sconto del 40% perché hai più di 65 anni "; 
+    //console.log("Prezzo scontato OVER 65:",finalPrice.toFixed(2), "€");
 }
 else {
     finalPrice = basicPrice; 
-    console.log("Prezzo pieno!, Non rispetti i requisiti per poter ricevere lo sconto"); 
-    console.log("Prezzo finale:", finalPrice.toFixed(2), "€");
+    message = "Prezzo pieno! Non rispetti i requisiti per poter ricevere lo sconto"; 
+    //console.log("Prezzo finale:", finalPrice.toFixed(2), "€");
 }
+
+// STAMPO 
+document.getElementById('ticket-price').innerHTML = 'Il prezzo del tuo biglietto è: ' + finalPrice.toFixed(2) + '€' + "<br> " +message;  
 
 
 
